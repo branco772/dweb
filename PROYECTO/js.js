7ffa5c09b88a0d76277aecf41bb3a3586329e8f9
branco772 <eduararratia@gmail.com>
2022-04-29 10:55:52 -0400

    u

---
+++ PROYECTO/js.js
@@ -0,0 +1,14 @@
+$(function(){
+    /*$('li').each(function(index, elemento){
+        $(this).css({
+            'top': '-200px'
+        });
+        $(this).animate({
+            'top': '0'
+        },2000 +(index*500));
+    });*/
+    $('nav').animate({
+        left: '10px'
+    }, 3000);
+    
+});
